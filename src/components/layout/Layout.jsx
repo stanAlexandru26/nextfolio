7455b7f00ddfc0dfb623bc11bb7c6766a984949c
stanAlexandru26 <stan.alexandru26@gmail.com>
2022-07-25 12:41:05 +0300

    feat: added basic tailwind preflight styles

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,4 @@
+export default function Layout({ children }) {
+  // Put Header or Footer Here
+  return <main className="layout">{children}</main>;
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
     <main>
-      <section className='bg-yellow-500'>
+      <section className=''>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
           <h1 className='mt-4'>Next.js + Tailwind CSS</h1>
+          <h2 className='mt-4'>Next.js + Tailwind CSS</h2>
+          <h3 className='mt-4'>Next.js + Tailwind CSS</h3>
+          <h4 className='mt-4'>Next.js + Tailwind CSS</h4>
+          <h5 className='mt-4'>Next.js + Tailwind CSS</h5>
+          <h6 className='mt-4 ' >Next.js + Tailwind CSS</h6>
 
           <footer className='absolute  bottom-2  text-gray-700'>
             {new Date().getFullYear()}
