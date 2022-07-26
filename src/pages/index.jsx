85e7a5ff19848ccceb2657726b4b957abaedd14b
stanAlexandru26 <stan.alexandru26@gmail.com>
2022-07-26 09:19:48 +0300

    feat: introduce strapi

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,56 @@
-export default function Home() {
-  return (
-    <section className=''>
-      <div className=' flex min-h-screen flex-col items-center justify-center text-center'>
-        <h1 className='mt-4'>Next.js + Tailwind CSS</h1>
-        <h2 className='mt-4'>Next.js + Tailwind CSS</h2>
-        <h3 className='mt-4'>Next.js + Tailwind CSS</h3>
-        <h4 className='mt-4'>Next.js + Tailwind CSS</h4>
-        <h5 className='mt-4'>Next.js + Tailwind CSS</h5>
-        <h6 className='mt-4 '>Next.js + Tailwind CSS</h6>
+import { fetchData } from '@/lib/fetchData';
 
-        <footer className='absolute  bottom-2  text-gray-700'>
-          {new Date().getFullYear()}
-        </footer>
+export default function Home({
+  projectData,
+  homePageData,
+  portfolioPageData,
+  contactData,
+}) {
+  console.log('🚀 ~ file: index.jsx ~ line 9 ~ contactData', contactData);
+  const { intro, headshot } = homePageData;
+  return (
+    <section>
+      <div className='flex flex-col-reverse items-start justify-between sm:flex-row'>
+        <div className='flex flex-col gap-4 pr-8'>
+          <h1>{contactData.name}</h1>
+          <p className='h4'>{contactData.profession}</p>
+          <p className='mb-12 text-gray-600 dark:text-gray-400 md:mb-16'>
+            {intro}
+          </p>
+        </div>
       </div>
     </section>
   );
 }
+
+export const getStaticProps = async ({ locale }) => {
+  const homePageRata = await fetchData('/homepage', {
+    populate: 'deep',
+    locale: locale,
+  });
+
+  const projectRes = await fetchData('/projects', {
+    populate: {
+      stacks: { populate: '*' },
+    },
+    locale: locale,
+    sort: ['order:asc'],
+  });
+  const portfolioPageRes = await fetchData('/portfolio', {
+    populate: '*',
+    locale: locale,
+  });
+  const contactRes = await fetchData('/contact', {
+    populate: 'deep',
+  });
+
+  return {
+    props: {
+      homePageData: homePageRata.data.attributes,
+      projectData: projectRes.data,
+      portfolioPageData: portfolioPageRes.data.attributes,
+      contactData: contactRes.data.attributes,
+    },
+    revalidate: 1,
+  };
+};
