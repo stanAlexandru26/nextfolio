baf4f7db20b119014b0cbaf44cf86141ee886500
stanAlexandru26 <stan.alexandru26@gmail.com>
2022-07-30 01:37:28 +0300

    feat: updated sitemap js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,11 +3,20 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://www.stefan-stan.dev */
   siteUrl: 'https://www.stefan-stan.dev',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
   },
+  alternateRefs: [
+    {
+      href: 'https://www.stefan-stan.dev/en',
+      hreflang: 'en',
+    },
+    {
+      href: 'https://www.stefan-stan.dev/ro',
+      hreflang: 'ro',
+    },
+  ],
+  exclude: ['/404'],
 };
